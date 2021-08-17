33e37012338712a89fdb11ce4d66872c7eb218b1
Jeremy Gillespie <jgillespie@additech.com>
2021-08-17 15:26:16 -0500

    Modified the Profile class to extend the user model. Now each "User" object can also be assigned the name, phone, email, org, and birthdate, attributes.

--- fusus/api/models.py
+++ fusus/api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Organization(models.Model):
     """
@@ -12,16 +14,28 @@ class Organization(models.Model):
     # State, Zip, etc.
 
 
-class User(models.Model):
+class Profile(models.Model):
     """
     This class represents the User object in the database.
     """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     # id <-This is created by Django as the primary key, nothing to add here.
-    name = models.CharField(max_length=50)  # Setting max_length to 50 to allow for First+Middle+Last names. Ideally
+    name = models.CharField(max_length=50, blank=True)  # Setting max_length to 50 to allow for First+Middle+Last names. Ideally
     # this would be separate fields.
-    phone = models.CharField(max_length=20)  # Setting max_length to 20 to allow for international numbers.
-    email = models.CharField(max_length=50, unique=True)  # unique=True will force validation on creation to confirm
+    phone = models.CharField(max_length=20, blank=True)  # Setting max_length to 20 to allow for international numbers.
+    email = models.CharField(max_length=50, unique=True, blank=True)  # unique=True will force validation on creation to confirm
     # that this field is unique among other User models.
-    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)  # Might consider not using on_delete so
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True)  # Might consider not using on_delete so
     # that we don't lose the user accounts when we delete the related Organization. (Many to one)
-    birthdate = models.DateField()
+    birthdate = models.DateField(null=True, blank=True)
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
