abaf1400aa53d272092798f6b715aa7d1abc2579
Jeremy Gillespie <jgillespie@additech.com>
2021-08-16 23:26:03 -0500

    Renaming endpoint to match instructions to /api/ from /fusus-api/.

--- fusus/fusus/urls.py
+++ fusus/fusus/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fusus_api/', include('fusus_api.urls')),
+    path('api/', include('api.urls')),
 ]
