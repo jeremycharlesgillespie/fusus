e0a8567ef65caa0386ed95d6c849893051b30724
Jeremy Gillespie <jgillespie@additech.com>
2021-08-17 15:26:40 -0500

    Added basic viewing/editing of the User/Profile attributes in the admin portal.

--- fusus/api/admin.py
+++ fusus/api/admin.py
@@ -1,1 +1,9 @@
 # Register your models here.
+from django.contrib.auth import admin
+from django.contrib import admin
+from api.models import Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Profile)
+
