7a882be751c268a2bbcf8222712bbf13b8745284
Jeremy Gillespie <jgillespie@additech.com>
2021-08-18 01:51:20 -0500

    Adding authentication class for simplejwt (standard setup). Adding basic permission class for view permission checking.

--- fusus/fusus/settings.py
+++ fusus/fusus/settings.py
@@ -137,7 +137,10 @@ USE_TZ = True
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ]
 }
