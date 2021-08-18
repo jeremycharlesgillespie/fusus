691de69f0eaeecd3253f4901e5e512839258d666
Jeremy Gillespie <jgillespie@additech.com>
2021-08-18 01:54:23 -0500

    Adding endpoint 'users/<int:id>/. This is complete. Adding endpoint 'users/'. WIP. Adding 2 endpoints for JWT token issue and refreshing, 'auth/login/' and 'auth/token/refresh/'

--- fusus/api/urls.py
+++ fusus/api/urls.py
@@ -8,5 +8,9 @@ from rest_framework_simplejwt.views import (
 app_name = "Fusus API"
 
 urlpatterns = [
-    path('test/', views.Test.as_view(), name='api-test')
+    path('test/', views.Test.as_view(), name='api-test'),
+    path('users/<int:id>/', views.GetUserByID.as_view(), name='api-get_user_by_id'),
+    path('users/', views.GetUsers.as_view(), name='api-get_user_by_id'),
+    path('auth/login/', EmailTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
