5769b4ebf0586fd3993e23dc3697f0879e6e0277
Jeremy Gillespie <jgillespie@additech.com>
2021-08-18 01:47:21 -0500

    Adding admin view for the Organization objects.

--- fusus/api/admin.py
+++ fusus/api/admin.py
@@ -1,9 +1,12 @@
 # Register your models here.
 from django.contrib.auth import admin
 from django.contrib import admin
-from api.models import Profile
+from api.models import Profile, Organization
 
 class ProfileAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Profile)
 
+class OrganizationAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Organization)
