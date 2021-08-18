81d430ad169b58af08e079538f3f763d756702f4
Jeremy Gillespie <jgillespie@additech.com>
2021-08-18 01:49:08 -0500

    Adding additional authentication backend so that we can authenticate with email+password.

--- fusus/fusus/settings.py
+++ fusus/fusus/settings.py
@@ -30,6 +30,8 @@ if ENV == "PROD":
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend',
+                           'api.backends.EmailBackend']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
