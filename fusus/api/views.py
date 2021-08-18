ee4eacf0c55394a83942f6085d4169b5ce6b6715
Jeremy Gillespie <jgillespie@additech.com>
2021-08-18 01:55:59 -0500

    Removing Test endpoint.

--- fusus/api/views.py
+++ fusus/api/views.py
@@ -2,7 +2,8 @@ from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-class Test(APIView):
+
+class EmailTokenObtainPairView(TokenObtainPairView):
     permission_classes = [permissions.AllowAny]
 
     def get(self, request):
