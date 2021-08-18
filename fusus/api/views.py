068805d8b622e51c7f88d4dd0d910130c6e77b67
Jeremy Gillespie <jgillespie@additech.com>
2021-08-18 01:59:02 -0500

    Adding permission IsAuthenticated and SimpleJWT view for getting a token. Bringing in the serializers from api.serializers for handling all responses. Adding models from api.models. These are needed for nearly all views.

--- fusus/api/views.py
+++ fusus/api/views.py
@@ -1,6 +1,12 @@
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from api.serializers import GetUserByIDSerializer, UserSerializer, OrganizationSerializer, ProfileSerializer, \
+    EmailTokenObtainPairSerializer
+from api.models import Organization, Profile
 
 
 class EmailTokenObtainPairView(TokenObtainPairView):
