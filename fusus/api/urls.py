12a3e7f2910af2180d9168e6e3f7b7e5d47cf39b
Jeremy Gillespie <jgillespie@additech.com>
2021-08-18 01:52:53 -0500

    Adding boilerplate views for simple_jwt.

--- fusus/api/urls.py
+++ fusus/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from . import views
-
+from .views import EmailTokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 app_name = "Fusus API"
 
 urlpatterns = [
